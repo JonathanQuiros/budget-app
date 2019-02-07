abab914a4c113ce3dcd36eddc05dcb255f39abf1
Jonathan Quiros Perez <jonathanquirosperez@jonathans-air.lan>
2019-02-07 13:13:20 -0800

    Initial commit

---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EditExpensePage } from '../../components/EditExpensePage';
+import expenses from '../fixtures/expenses';
+
+let editExpense, removeExpense, history, wrapper;
+const mockExpense = expenses[2];
+
+beforeEach(() => {
+  editExpense = jest.fn();
+  removeExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(
+    <EditExpensePage
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+      expense={mockExpense}
+    />
+  );
+});
+
+test('should render EditExpensePage', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(mockExpense);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(mockExpense.id, mockExpense);
+});
+
+test('should handle removeExpense', () => {
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({ id: mockExpense.id });
+});
